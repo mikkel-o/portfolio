77990c9c14721f76dc7bd9230ccd0f31024f5165
mikkel-o <85589492+mikkel-o@users.noreply.github.com>
2022-08-11 18:11:55 +0200

    fourth commit

---
+++ routes/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet, Link } from "react-router-dom";
+
+export default function Projects() {
+
+  return (
+    <div className={''}>
+     
+      <div className={'project__wrapper'}>
+        <p>PROJECTS</p>
+    </div>
+  </div> /* end .wrapperz */
+  );
+}
+
