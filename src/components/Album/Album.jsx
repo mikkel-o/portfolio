661d8a68d321c3be6e3ffd0101fdc5ec9f75e567
mikkel-o <85589492+mikkel-o@users.noreply.github.com>
2023-07-03 02:26:18 +0200

    layout phone

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -1,21 +1,76 @@
-import React from "react";
+import React, { useState }  from "react";
 import { VideoModal } from '../VideoModal/VideoModal';
 import { AlbumContainer} from "./AlbumContainer";
 import { AlbumCard} from "./AlbumCard";
 import { AlbumCardOverlay} from "./AlbumCardOverlay";
 import './Album.css';
-
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {MoreButton, PlayButton} from "../Buttons/Buttons";
+import { 
+  addProject, 
+  clearProjects, 
+  projectCoord, 
+  addId, 
+  clearId, 
+  addLink, 
+  clearLink } from "../../features/singleProject/singleProjectSlice";
 
 export function Album(props) {
   const {items, allItems, filters, type, overlay, layout, scroll } = props;
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  
 
+  const onClickyHandler = (event, item) => { 
+    setIsOpen(!isOpen);
+    const childPos = event.target.parentElement.parentElement.parentElement.getBoundingClientRect();
+    const index = [...event.target.parentElement.parentElement.parentElement.parentElement.children].indexOf(event.target.parentElement.parentElement.parentElement);
+    const coord = [childPos.x, childPos.y, childPos.width, childPos.height, index];
+    dispatch(clearProjects());
+    dispatch(addProject(item));
+    dispatch(projectCoord(coord));
+  }
+  
+  const onClickHandlerPlay = (event, item, activeFilmIndex) => { 
+    dispatch(clearId());
+    dispatch(clearLink());
+    dispatch(addId(item.id));
+    setTimeout(() => {
+      item.album ? dispatch(addLink(item.album[activeFilmIndex].embed)) : dispatch(addLink(item.embed));  
+    }, "500")  
+  } 
   return (
     <>
         <AlbumContainer scroll={scroll} layout={layout}>
           {items.map((item, i) => (
-            <AlbumCard item={item} index={i} key={item.id} allItems={allItems} type={type} scroll={scroll} layout={layout}> 
-              {overlay ? <AlbumCardOverlay item={item} filters={filters}/> : null}
+          <>
+          <AlbumCard item={item} index={i} key={item.id} allItems={allItems} type={type} scroll={scroll} layout={layout}> 
+              {overlay ? <AlbumCardOverlay item={item} key={item.id + 44} filters={filters}/> : null}
             </AlbumCard>
+            <div key={item.id + 55} className={"test"}>
+            {overlay ? 
+            <div key={item.id + 66} className={"album__overlay-wrapper album__overlay-wrapper--scroll"}>
+      <Link 
+        to={
+            item.album ? 
+              `/work/${item.name}/${item.album[item.activeFilmIndex ? item.activeFilmIndex : 0].name}?filters=${filters ? filters.map(e => e.value + '%2C') : ''}`
+            :
+              `/work/${item.name}?filters=${filters ? filters.map(e => e.value + '%2C') : ''}`
+            } 
+        onClick={event => onClickyHandler(event, item)}
+      >
+        <MoreButton/>
+      </Link>
+      
+      <div onClick={event => onClickHandlerPlay(event, item, item.activeFilmIndex ? item.activeFilmIndex : 0)}>
+        <PlayButton/>
+      </div>
+      
+      
+    </div> : null}
+          </div>
+          </>
           ))}
           <VideoModal/>
       </AlbumContainer>
--- src/components/Album/AlbumCardOverlay.jsx
+++ src/components/Album/AlbumCardOverlay.jsx
@@ -1,39 +1,8 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import {MoreButton, PlayButton} from "../Buttons/Buttons";
-import { 
-  addProject, 
-  clearProjects, 
-  projectCoord, 
-  addId, 
-  clearId, 
-  addLink, 
-  clearLink } from "../../features/singleProject/singleProjectSlice";
+import React from "react";
 
 export function AlbumCardOverlay(props) {
-  const {item, filters } = props; 
-  const dispatch = useDispatch();
-  const [isOpen, setIsOpen] = useState(false);
-  
-  const onClickyHandler = (event) => { 
-    setIsOpen(!isOpen);
-    const childPos = event.target.parentElement.parentElement.parentElement.getBoundingClientRect();
-    const index = [...event.target.parentElement.parentElement.parentElement.parentElement.children].indexOf(event.target.parentElement.parentElement.parentElement);
-    const coord = [childPos.x, childPos.y, childPos.width, childPos.height, index];
-    dispatch(clearProjects());
-    dispatch(addProject(item));
-    dispatch(projectCoord(coord));
-  }
-  
-  const onClickHandlerPlay = (event, activeFilmIndex) => { 
-    dispatch(clearId());
-    dispatch(clearLink());
-    dispatch(addId(item.id));
-    setTimeout(() => {
-      item.album ? dispatch(addLink(item.album[activeFilmIndex].embed)) : dispatch(addLink(item.embed));  
-    }, "500")  
-  } 
+  const {item } = props; 
+
 
   return (  
     <>
@@ -43,30 +12,10 @@ export function AlbumCardOverlay(props) {
     <h2 className="album__overlay-title album__overlay-title--scroll">{item.album ? item.album[item.activeFilmIndex ? item.activeFilmIndex : 0].title : item.title ? item.title : ""}</h2>
     <h3 className="album__overlay-subtitle album__overlay-subtitle--scroll">{item.role ? item.role.map(element => element).join(' | ') : ""}</h3> 
     </div>
-    <div className={"album__overlay-wrapper album__overlay-wrapper--scroll"}>
-      <Link 
-        to={
-            item.album ? 
-              `/work/${item.name}/${item.album[item.activeFilmIndex ? item.activeFilmIndex : 0].name}?filters=${filters ? filters.map(e => e.value + '%2C') : ''}`
-            :
-              `/work/${item.name}?filters=${filters ? filters.map(e => e.value + '%2C') : ''}`
-            } 
-        onClick={onClickyHandler}
-      >
-        <MoreButton/>
-      </Link>
-      
-      <div onClick={event => onClickHandlerPlay(event, item.activeFilmIndex ? item.activeFilmIndex : 0)}>
-        <PlayButton/>
-      </div>
-      
-      
-    </div>
+    
     <div className={'album__overlay-bg album__overlay-bg--scroll'}></div>
     </div>
-    <div className={"test"}>
-      TEST
-    </div>
+    
     </>
   );
 };
