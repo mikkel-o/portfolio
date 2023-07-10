9143091f362d86e707d7aaf6b723e780e5bcb62a
mikkel-o <85589492+mikkel-o@users.noreply.github.com>
2023-07-10 02:12:27 +0200

    phone layout

---
+++ src/components/Icons/Icon.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ReactComponent as LayoutFullSolid } from './layout-full-solid.svg';
+import { ReactComponent as LayoutOneSolid } from './layout-one-solid.svg';
+import { ReactComponent as LayoutTwoSolid } from './layout-two-solid.svg';
+import { ReactComponent as LayoutThreeSolid } from './layout-three-solid.svg';
+
+
+const Icon = ({ name, color, size }) => (
+  name === "layout-full-solid" ? 
+    <LayoutFullSolid fill={color} width={size} height={size}/> 
+  : 
+    name === "layout-one-solid" ? 
+      <LayoutOneSolid fill={color} width={size} height={size}/> 
+    : 
+      name === "layout-two-solid" ? 
+        <LayoutTwoSolid fill={color} width={size} height={size}/> 
+      : name === "layout-three-solid" ? 
+          <LayoutThreeSolid fill={color} width={size} height={size}/> 
+        : 
+          null
+    
+);
+
+
+
+export default Icon;
