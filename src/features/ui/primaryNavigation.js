47f1237d5460d5e4326d6aca7665a2ce4a6419e1
mikkel-o <85589492+mikkel-o@users.noreply.github.com>
2023-07-14 03:39:21 +0200

    ui layout

---
+++ src/features/ui/primaryNavigation.js
@@ -0,0 +1,156 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import './primaryNavigation.css';
+import { Toggle} from "../../components/Toggle";
+import { toggle } from "../../components/toggleSlice";
+import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
+
+/*- - - - - - -- - - - - - - - - - - - - - - - - - - - - - - -*\
+|                        ANIMATION                             |
+\*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// Constants
+const transition = {duration: .2, ease: [0.3, 0.13, 0.13, 0.96]}
+
+
+// Variants
+const burgerMenu = [
+  { 
+    //initial: {y: -4, rotate: 0},
+    animate: {y: 0, rotate: 0},
+    hover: { y: -4, transition: transition },
+  },
+  { 
+    animate: {scale: 0}, 
+    hover: { scale: 1, opacity: 1, transition: {duration: .1, ease: [0.3, 0.13, 0.13, 0.96]} },
+  },
+  { 
+    //initial: {y: 4, rotate: 0},
+    animate: {y: 0, rotate: 0},
+    hover: { y: 4, transition: transition }, 
+  }
+];
+const burgerMenuExit = [
+  { 
+    //initial: {y: -4, rotate: 0},
+    animate: {y: 6, rotate: 45, transition: {duration: .2, ease: [0.3, 0.13, 0.13, 0.96]} },
+    //animate: {y: 0, transition: {duration: .2, rotate: 45, ease: [0.3, 0.13, 0.13, 0.96]} },
+    hover: { y: 6, rotate: 0, transition: transition },
+    //exit: {y: 0, opacity: 0, transition: {duration: 1, ease: [0.3, 0.13, 0.13, 0.96]} },
+  },
+  { 
+    animate: {scale: 0}, 
+    hover: { scale: 1, opacity: 0, transition: {duration: .1, ease: [0.3, 0.13, 0.13, 0.96]} },
+    //exit: {y: 0, opacity: 0, transition: {duration: 1, ease: [0.3, 0.13, 0.13, 0.96]} },
+  },
+  { 
+    //initial: {y: 4, rotate: 0 },
+    animate: {y: -6, rotate: -45, transition: {duration: .2, ease: [0.3, 0.13, 0.13, 0.96]} },
+   // animate: {y: 0, transition: {duration: .2, rotate: 45, ease: [0.3, 0.13, 0.13, 0.96]} },
+    hover: {y: -7, rotate: 0, transition: transition }, 
+    //exit: {y: 0, opacity: 0, transition: {duration: 1, ease: [0.3, 0.13, 0.13, 0.96]} },
+  }
+];
+
+
+
+const PrimaryNavigation = (props) => {
+  const {children} = props;
+  let isActive = false;
+  const dispatch = useDispatch();
+      
+  const onClickToggleNavigation = (event, name) => { 
+        if (event.target.classList.contains('active')) {
+          event.target.classList.remove('active');
+          isActive = false;
+        } else {
+          event.target.classList.add('active');
+          isActive = true;
+        }
+          dispatch(toggle(name))
+      }
+        
+const menuToggle = useSelector(state => state.toggle['primary_menu']);
+
+let currentLocation = String(useLocation().pathname),
+backLocation = currentLocation.substring(0, currentLocation.lastIndexOf("/"));
+
+  /*
+    let currentLocation = String(useLocation().pathname),
+    backLocation = currentLocation.substring(0, currentLocation.lastIndexOf("/"));*/
+
+
+  return (
+    <>
+    <div className={'main-nav'}>
+
+
+    {/* toggle menu button */}
+    <motion.button
+      initial={'initial'}
+      animate={'animate'}
+      exit={'exit'}
+      whileHover={'hover'}
+      transition={'transition'}
+      className={(isActive) ? 'main-nav__toggle-button active' : 'main-nav__toggle-button'} 
+      onClick={event => onClickToggleNavigation(event, 'primary_menu')} >
+      <motion.span variants={menuToggle ? burgerMenuExit[0]:  burgerMenu[0]}>|</motion.span>
+      <motion.span variants={menuToggle ? burgerMenuExit[1]:  burgerMenu[1]}>|</motion.span>
+      <motion.span variants={menuToggle ? burgerMenuExit[2]:  burgerMenu[2]}>|</motion.span>
+      
+    </motion.button> 
+    
+
+    <Toggle id={'primary_menu'}>
+    {/* main menu */}
+    <div className={(isActive === false) ? 'main-nav__wrapper main-nav__wrapper--hidden' : 'main-nav__wrapper main-nav__wrapper--show'}>      
+      <ul className={'main-nav__list'}>
+        <li className={'main-nav__item'}>
+          <Link to={'/about'} onClick={event => onClickToggleNavigation(event, 'primary_menu')} >about</Link>
+        </li>
+        {/*<li className={'main-nav__item'}>
+          <Link to={'/showreel'} onClick={event => onClickToggleNavigation(event, 'primary_menu')} >showreel</Link>
+        </li>*/}
+        <li className={'main-nav__item'}>
+          <Link to={'/work/categories'} onClick={event => onClickToggleNavigation(event, 'primary_menu')} >work</Link>
+        </li>
+        
+        <li className={'main-nav__item'}>
+          <Link to={'/photo'} onClick={event => onClickToggleNavigation(event, 'primary_menu')} >photo</Link>
+        </li>
+        
+        
+      </ul>  
+    </div>
+    </Toggle>
+  
+
+    
+
+
+ 
+
+{children}
+  
+  
+{/* END .main-nav-wrapper */}
+</div>
+<div className={'bottom-nav'}>
+       {/* navigate back button */}
+    
+       <Link to={`${backLocation}`} className={'back__link'}>
+        <button className={currentLocation === '/' ? `hide back__button` : `back__button`}>
+          <img alt={'go back'} src={'/icons/left-arrow.png'} />
+        </button>
+      </Link> 
+</div>
+</> 
+  );
+};
+
+export default PrimaryNavigation;
+
+
+
+// add this for counts: <span className={'filter-count'}>{filter.countFilter} / {filter.countTotal}</span>
---
+++ src/features/ui/secondaryNavigation.js
@@ -0,0 +1,141 @@
+import React, { useState, useRef, useEffect  } from "react";
+import './secondaryNavigation.css';
+import Icon from "../../components/Icons/Icon";
+import {useDispatch, useSelector} from "react-redux"
+import { layout } from "../../components/toggleSlice";
+
+
+const SecondaryNavigation = (props) => {
+  const {children} = props;
+ 
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+  const layoutIsh = useSelector(state => state.toggle.layout);
+  console.log(layoutIsh);
+  const onClickHandler = (event) => { 
+    setIsOpen(!isOpen);
+  }
+  const onClickBtnHandler = (event, number) => { 
+    console.log(number);
+    dispatch(layout(number));
+    console.log(layoutIsh);
+    setIsOpen(false);
+    setIsMenuOpen(false);
+  }
+  const onClickToggleHandler = () => { 
+    
+    
+    
+    setIsMenuOpen(!isMenuOpen);
+  }
+  const ref = useRef();
+
+  useOnClickOutside(ref, () => {setIsOpen(false)}); // Detect click outside filters box (try and put in seperat hook so it can be reused)
+  
+
+  return (
+    <div className={`${isMenuOpen ? 'sec-nav__wrapper--open' : 'sec-nav__wrapper--closed'} sec-nav__wrapper`}>
+      <div className={"sec-nav__toggle"}>
+        <button 
+          className={"sec-nav__btn"}
+          onClick={event => onClickToggleHandler(event)}
+        >
+          <div className={'sec-nav__dot sec-nav__dot-one'}></div>
+          <div className={'sec-nav__dot sec-nav__dot-two'}></div>
+          <div className={'sec-nav__dot sec-nav__dot-three'}></div>
+        </button>
+      </div>
+      <div className={'sec-nav__menu'}>
+      <div className={'layout-btn__wrapper'} ref={ref}>
+     <button 
+      className={'btn__layout--toggle btn__layout btn__layout--active'}
+      onClick={event => onClickHandler(event)}
+      >
+     {
+      layoutIsh === 0 ? 
+        <Icon name={'layout-full-solid'} size={26} color={''}></Icon> 
+      :
+      layoutIsh === 1 ? 
+        <Icon name={'layout-one-solid'} size={28} color={''}></Icon> 
+      :
+      layoutIsh === 2 ? 
+        <Icon name={'layout-two-solid'} size={28} color={''}></Icon> 
+      :
+        <Icon name={'layout-three-solid'} size={28} color={''}></Icon> 
+      
+      }
+     
+    </button>
+    
+    <div className={`bnt__layout-wrapper ${isOpen ? 'bnt__layout-wrapper--active' : ''}` }>
+    
+    <button 
+      className={'btn__layout'}
+      onClick={event => onClickBtnHandler(event, 0)}
+    >
+    <Icon name={'layout-full-solid'} size={26} color={''}></Icon>
+    </button>
+    <button 
+      className={'btn__layout'}
+      onClick={event => onClickBtnHandler(event, 1)}
+    >
+    <Icon name={'layout-one-solid'} size={28} color={''}></Icon>
+    </button>
+    <button 
+      className={'btn__layout'}
+      onClick={event => onClickBtnHandler(event, 2)}
+    >
+    <Icon name={'layout-two-solid'} size={28} color={''}></Icon>
+    </button>
+    <button 
+      className={'btn__layout'}
+      onClick={event => onClickBtnHandler(event, 3)}
+    >
+    <Icon name={'layout-three-solid'} size={28} color={''}></Icon>
+    </button>
+    </div>
+    {children}
+    </div>
+      
+      </div>
+    </div> 
+  );
+};
+
+export default SecondaryNavigation;
+
+
+
+// add this for counts: <span className={'filter-count'}>{filter.countFilter} / {filter.countTotal}</span>
+
+
+
+function useOnClickOutside(ref, handler) {
+  
+  useEffect(
+    () => {
+      const listener = (event) => {
+        // Do nothing if clicking ref's element or descendent elements
+        if (!ref.current || ref.current.contains(event.target)) {
+          
+          return;
+        }
+        handler(event);
+      };
+      document.addEventListener("mousedown", listener);
+      document.addEventListener("touchstart", listener);
+      return () => {
+        document.removeEventListener("mousedown", listener);
+        document.removeEventListener("touchstart", listener);
+      };
+    },
+    // Add ref and handler to effect dependencies
+    // It's worth noting that because passed in handler is a new ...
+    // ... function on every render that will cause this effect ...
+    // ... callback/cleanup to run every render. It's not a big deal ...
+    // ... but to optimize you can wrap handler in useCallback before ...
+    // ... passing it into this hook.
+    [ref, handler]
+  );
+}
