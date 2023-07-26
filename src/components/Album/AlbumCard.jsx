3fed145a999a1fde046792a354de79f4bb430daa
mikkel-o <85589492+mikkel-o@users.noreply.github.com>
2023-07-27 01:21:10 +0200

    project album

--- src/components/Album/AlbumCard.jsx
+++ src/components/Album/AlbumCard.jsx
@@ -193,6 +193,7 @@ export function AlbumCard(props) {
                     loop
                     
                     
+                    playsInline
                     muted
                    
                     onMouseOver=
